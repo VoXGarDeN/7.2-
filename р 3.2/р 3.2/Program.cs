14bab0a9304af640a1b6df066b93a770dfb3bce5
VoXGarDeN <153168997+VoXGarDeN@users.noreply.github.com>
2023-12-07 10:53:16 +0300

    кайф

---
+++ р 3.2/р 3.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Создание массива строк с именами пользователей
+        string[] users = { "Егор", "Егор", "Егор", "Егор" }; /// нельзя использовать "string[] vowels1 = { "a", "e", "i", "o", "u" };"
+
+        // Вывод списка имен пользователей
+        foreach (string user in users)
+        {
+            Console.WriteLine(user);
+        }
+    }
+}
